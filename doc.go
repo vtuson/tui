db855b2d8f66e55a5882a7e8a10c3bfcec473e56
victor palau <victorpalau@bitnami.com>
2018-01-15 21:01:18 +0100

    fix docs

--- doc.go
+++ doc.go
@@ -1,9 +1,9 @@
-//This is a Basic Implementation of a text ui that can be used to run CLI commands or you can define your own HandlerCommand to run Go functions
-//Basic Structure is:
-//	Menu
-//		[]Command
-//			[]Args
-//
-//By default a CLI command handler is provided that is able to pass parameters as flag, options or Enviroment variables
+// This is a Basic Implementation of a text ui that can be used to run CLI commands or you can define your own HandlerCommand to run Go functions
+// Basic Structure is:
+// 	Menu
+// 		[]Command
+// 			[]Args
+// 
+// By default a CLI command handler is provided that is able to pass parameters as flag, options or Enviroment variables
 
 package tui
