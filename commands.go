a457558abf5433c445d4c21e5d6ff5aab189f6dd
victor palau <victorpalau@bitnami.com>
2018-01-15 17:30:01 +0100

    fix docs

--- commands.go
+++ commands.go
@@ -7,15 +7,6 @@ import (
 	"os/exec"
 )
 
-/*
-This is a Basic Implementation of a text ui that can be used to run CLI commands or you can define your own HandlerCommand to run Go functions
-Basic Structure is:
-	Menu
-		[]Command
-			[]Args
-
-By default a CLI command handler is provided that is able to pass parameters as flag, options or Enviroment variables
-*/
 type Command struct {
 	Title       string
 	Description string
